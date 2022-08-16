0bf18a37645b0c959a0eca9ebf0f55eb5f08d0cd
Kai Chen (Spark AR) <ckai@fb.com>
2022-08-16 15:07:27 -0500

    Integrate ImGui

--- src/Application.cpp
+++ src/Application.cpp
@@ -16,6 +16,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 int main(void)
 {
     GLFWwindow *window;
@@ -58,10 +62,10 @@ int main(void)
     }
 
     float positions[] = {
-        100.0f,  100.0f, 0.0f, 0.0f, // 0
-        200.0f,  100.0f, 1.0f, 0.0f, // 1
-        200.0f,  200.0f, 1.0f, 1.0f, // 2
-        100.0f,  200.0f, 0.0f, 1.0f, // 3
+        100.0f, 100.0f, 0.0f, 0.0f, // 0
+        200.0f, 100.0f, 1.0f, 0.0f, // 1
+        200.0f, 200.0f, 1.0f, 1.0f, // 2
+        100.0f, 200.0f, 0.0f, 1.0f, // 3
     };
 
     unsigned int indices[] = {
@@ -83,14 +87,12 @@ int main(void)
 
     glm::mat4 proj = glm::ortho(0.0f, (float)width, 0.0f, (float)height, -1.0f, 1.0f);
     glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-    glm::mat4 mvp = proj * view * model;
 
     // Shader shader("res/shaders/basic-uniform.shader");
     Shader shader("res/shaders/texture-basic.shader");
     shader.bind();
-    shader.setUniformMat4f("u_MVP", mvp);
+
 
     Texture texture("res/textures/opengl.png");
     texture.bind();
@@ -98,15 +100,30 @@ int main(void)
 
     Renderer renderer;
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+    ImGui::StyleColorsDark();
+
     // float red = 0.0f;
     // float increment = 0.05f;
-
+    glm::vec3 translation(200, 200, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         renderer.clear();
         // shader.setUniform4f("u_Color", red, 0.3f, 0.8f, 1.0f);
+
+        glfwPollEvents();
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+        glm::mat4 mvp = proj * view * model;
+        shader.setUniformMat4f("u_MVP", mvp);
 
         renderer.draw(va, ib, shader);
 
@@ -120,6 +137,16 @@ int main(void)
         //     increment = 0.05;
         // }
 
+        {
+            ImGui::Begin("Debugging"); // Create a window called "Hello, world!" and append into it.
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::SliderFloat("Translation.x", &translation.x, 0.0f, width);
+            ImGui::SliderFloat("Translation.y", &translation.y, 0.0f, height);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         /* Swap front and back buffers */
         GLCall(glfwSwapBuffers(window));
 
@@ -127,6 +154,11 @@ int main(void)
         GLCall(glfwPollEvents());
     }
 
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
